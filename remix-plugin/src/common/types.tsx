5ce7b66abee0205097ae577158eaa8203008aadc
Darko <darko.macesic@digacon-software.com>
2019-08-21 15:22:41 +0200

    Implement remix resolver

--- remix-plugin/src/common/types.tsx
+++ remix-plugin/src/common/types.tsx
@@ -1,3 +1,8 @@
+export interface ResolverResult {
+    source: string,
+    location: string
+}
+
 export interface Resolver {
-    resolve(location: string, path: string): Promise<string>;
+    resolve(location: string, path: string): Promise<ResolverResult>;
 }
--- remix-plugin/src/remix/remix-resolver.ts
+++ remix-plugin/src/remix/remix-resolver.ts
@@ -1,10 +1,27 @@
-import { Resolver } from '../common/types';
+import { Resolver, ResolverResult } from '../common/types';
+import { remixClient } from './remix-client';
 
 export default class RemixResolver implements Resolver {
 
-    resolve = (location: string, path: string): Promise<string> => {
-        return new Promise<string>((resolve, reject) => {
-            // todo: resolve logic
+    private extension: string = '.code';
+
+    resolve = (location: string, path: string): Promise<ResolverResult> => {
+        return new Promise<ResolverResult>(async (resolve, reject) => {
+            const _path = path.replace('./', '');
+            try {
+                let source = await remixClient.getFile(this.getBrowserPath(_path));
+                resolve({ source, location: _path } as ResolverResult);
+            } catch (error) {
+                reject(error);
+            }
         });
+    }
+
+    private getBrowserPath = (path: string) => {
+        let browserPath = `browser/${path}`;
+        if (path.endsWith(this.extension)) {
+            return browserPath;
+        }
+        return browserPath.concat(this.extension);
     }
 }
