a5f666492f6b873ff2d251cd9922ebc29d2199d7
Darko <darko.macesic@digacon-software.com>
2019-09-09 17:54:10 +0200

    Add array support for argument parsing

--- remix-plugin/src/sections/compute-witness/parser.ts
+++ remix-plugin/src/sections/compute-witness/parser.ts
@@ -1,6 +1,6 @@
 
 export function parseArguments(source: string) {
-    var regex = /field(?:\[[0-9]+\])?\s+([a-zA-Z0-9_]+)/g
+    var regex = /field(?:\[([0-9]+)\])?\s+([a-zA-Z0-9_]+)/g;
     var lines = source.split('\n');
 
     let args = [];
@@ -11,7 +11,15 @@ export function parseArguments(source: string) {
             
         let match: RegExpExecArray;
         while (match = regex.exec(lines[i])) {
-            args.push(match[1]);
+            if (match[1]) {
+                let arraySize = Number(match[1]);
+                let array = [...Array(arraySize).keys()];
+
+                let varName = match[2];
+                array.forEach(index => args.push(`${varName}_${index}`));
+            } else {
+                args.push(match[2]);
+            }      
         }
         break;
     }
