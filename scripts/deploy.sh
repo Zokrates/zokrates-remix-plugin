8d1696f4ed422d0b2d9b31c94f51c4b18c8a8d09
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-09-24 15:19:01 +0200

    Fixing typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,10 @@ if [ "$CIRCLE_BRANCH" == "master" ]; then
 fi
 echo $TAG
 ssh -o "StrictHostKeyChecking no" circleci@zokrates.blockchain-it.hr "\
-echo 'y' | docker system prune \
-rm -rf /home/circleci/zokrates-remix-plugin || true \
-cd /home/circleci \
-git clone https://github.com/blockchain-it-hr/zokrates-remix-plugin \
-cd /home/circleci/zokrates-remix-plugin && git checkout ${CIRCLE_BRANCH} \
-docker pull blockchainit/zokrates-remix-plugin:${TAG} \
+echo 'y' | docker system prune && \
+rm -rf /home/circleci/zokrates-remix-plugin || true && \
+cd /home/circleci && \
+git clone https://github.com/blockchain-it-hr/zokrates-remix-plugin && \
+cd /home/circleci/zokrates-remix-plugin && git checkout ${CIRCLE_BRANCH} && \
+docker pull blockchainit/zokrates-remix-plugin:${TAG} && \
 docker-compose up -d"
