43373093e63e19d499c1fae8774d1d3e002c1180
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-09-24 15:32:16 +0200

    Updating deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,8 +11,8 @@ echo $TAG
 ssh -o "StrictHostKeyChecking no" circleci@zokrates.blockchain-it.hr "\
 echo 'y' | docker system prune && \
 rm -rf /home/circleci/zokrates-remix-plugin || true && \
-cd /home/circleci && \
-git clone https://github.com/blockchain-it-hr/zokrates-remix-plugin && \
-cd /home/circleci/zokrates-remix-plugin && git checkout ${CIRCLE_BRANCH} && \
+mkdir -p /home/circleci/zokrates-remix-plugin && \
+cd /home/circleci/zokrates-remix-plugin && \
+curl https://raw.githubusercontent.com/blockchain-it-hr/zokrates-remix-plugin/${CIRCLE_BRANCH}/docker-compose.yml > docker-compose.yml && \
 docker pull blockchainit/zokrates-remix-plugin:${TAG} && \
 docker-compose up -d"
