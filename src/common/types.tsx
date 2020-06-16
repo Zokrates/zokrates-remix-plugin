11b012ec20f249919e2e384733579dae2d3f9ed7
dark64 <darem966@gmail.com>
2020-06-16 22:25:23 +0200

    remove ci configurations

--- src/common/types.tsx
+++ src/common/types.tsx
@@ -1,7 +1,7 @@
 import { ResolverResult } from 'zokrates-js';
 
-export interface Resolver {
+interface Resolver {
     resolve(location: string, path: string): Promise<ResolverResult>;
 }
 
-export type ResolverResult = ResolverResult;
+export { Resolver, ResolverResult };
